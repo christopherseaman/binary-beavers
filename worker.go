b99ad0e32dec1061a14cde3ba9543be34376a60a
Christopher Seaman <86775+christopherseaman@users.noreply.github.com>
2024-03-19 11:42:26 -0700

    Use big's ProbablyPrime(1) (known good through int64 max)

--- worker.go
+++ worker.go
@@ -5,38 +5,53 @@ import (
 	"encoding/json"
 	"math/big"
 	"net/http"
-
-	"github.com/fxtlabs/primes"
 )
 
 // Parameters for g, g^a, and p
 var (
-	g     = big.NewInt(5)
+	g     = int64(5)
 	g_a   = big.NewInt(27)
 	p     = big.NewInt(443)
-	bound = big.NewInt(31)
+	bound = int64(31)
 	// g_a  = big.NewInt(244241057144443665472449725715508406620552440771362355600491)
 	// p   = big.NewInt(3217014639198601771090467299986349868436393574029172456674199)
-	// bound = big.NewInt(3569817792505)
+	// bound = int64(3569817792505)9223372036854775807
 )
 
 // FactorBase returns a slice of prime numbers up to a given bound
-func FactorBase() []*big.Int {
-	return primes.Sieve(bound)
-}
+// FactorBase returns a slice of prime numbers up to a given bound
+func FactorBase() []int64 {
+	var factorBase []int64
 
-// Relation represents a relation to be sent to the collector
-type Relation struct {
-	Data string `json:"data"` // Adjust according to expected format
-}
+	// Iterate from 2 to the bound and check if each number is prime
+	for i := int64(2); i <= bound; i++ {
+		// Create a big.Int from the current number to use the ProbablyPrime method
+		x := big.NewInt(i)
 
-func main() {
-	// Example: Generate and send a relation
-	relation := Relation{Data: "example_relation_data"}
-	if err := sendRelation(relation); err != nil {
-		// Handle error
-		panic(err)
+		// Check if the number is probably prime
+		if x.ProbablyPrime(1) {
+			factorBase = append(factorBase, i)
+		}
 	}
+
+	return factorBase
+}
+
+type Relation map[int64]int
+
+// generateRelation generates a relation using the quadratic sieve
+func generateRelation(g int64, g_a, p *big.Int, factorBase []int64) Relation {
+	// IDEA: min k st g^k > to bound and (g^k)^2 - h^2 mod p to generate relations
+	// Placeholder for actual relation generation logic
+	// This function should:
+	// 1. Choose random h < bound and compute (g+h)(g-h) mod p
+	// 2. Attempt to factor (g^j+h)(g^k-h) using the factor base sv1
+	// 3. Factor (g^k+h) and (g^k-h) over the factor base as sv2 and sv3
+	// 4. Save the relation sv1 - sv2 - sv3 in a map
+	// Note: -1 == p-2 because exponents are mod (p-1)
+
+	// Placeholder return value
+	return Relation{2: 3, 3: 1} // Example with factored 2^3 * 3^1
 }
 
 // sendRelation sends a relation to the collector
@@ -56,4 +71,13 @@ func sendRelation(relation Relation) error {
 	// ...
 
 	return nil
+}
+
+func main() {
+	// FIXME: Implement generateRelation using the quadratic sieve
+	relation := generateRelation(g, g_a, p, FactorBase())
+	if err := sendRelation(relation); err != nil {
+		// Handle error
+		panic(err)
+	}
 }
